84035ac8958d2d67372088bfcafc6ed5564c7293
daniya12345669 <siddiquiidaniya1@gmail.com>
2025-11-12 20:13:38 +0530

    Create funtion.py

---
+++ funtion.py
@@ -0,0 +1,10 @@
+def anyone(x,y):
+ name=input("enter you name")
+ age=int(input("enter your age"))
+ rollnumber=int(input("enter your rollnumber"))
+ addition=(age+rollnumber)
+ print("the sum is:",addition)
+ mul=x*y
+ print("multiplication is",mul)
+ return name,age,rollnumber
+ anyone(5,10)
